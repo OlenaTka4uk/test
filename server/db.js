010e709414af07330348474a502912560105e0d1
OlenaTka4uk <bushueva07@gmail.com>
2024-10-20 10:11:09 -0400

    change file

--- server/db.js
+++ server/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI =  "mongodb://root:<your-password>@127.0.0.1:27017";
+const mongoURI =  "mongodb://root:<OoimGQcPx0NzKfX3fcMGLoJi>@172.21.81.152:27017";
 
 const connectToMongo = async (retryCount) => {
     const MAX_RETRIES = 3;
